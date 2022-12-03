95f48df7c146d81845fc8e85a576186f1f3179b8
Krunoslav Husak <kruno@husak.me>
2022-12-03 15:11:48 +0100

    Added updated at to weather data

--- application/weather.go
+++ application/weather.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"net/url"
+	"time"
 
 	"github.com/shopspring/decimal"
 )
@@ -25,10 +26,15 @@ type Weather struct {
 	IndoorTemperature decimal.Decimal `json:"indoor_temperature"`
 	IndoorHumidity    int             `json:"indoor_humidity"`
 	Pressure          decimal.Decimal `json:"pressure"`
+	UpdatedAt         time.Time       `json:"updated_at"`
 }
 
 func NewWeatherFromStation(data string) (*Weather, error) {
 	d, err := url.ParseQuery(data)
+	if err != nil {
+		return nil, err
+	}
+	t, err := time.Parse("2006-01-02 15:04:05", d.Get("dateutc"))
 	if err != nil {
 		return nil, err
 	}
@@ -52,5 +58,6 @@ func NewWeatherFromStation(data string) (*Weather, error) {
 		IndoorTemperature: convertFahrenheitToCelsius(strToFloat(d.Get("indoortempf"))),
 		IndoorHumidity:    strToInt(d.Get("indoorhumidity")),
 		Pressure:          convertHGToKPA(strToFloat(d.Get("baromin"))),
+		UpdatedAt:         t,
 	}, nil
 }
