d76fd0f55a40c176dabf8c27651c36b31e941ef0
Krunoslav Husak <kruno@husak.me>
2022-12-23 10:28:32 +0100

    Added api prefix to API endpoints

--- application/router.go
+++ application/router.go
@@ -5,8 +5,10 @@ import "net/http"
 func (app *Application) NewRouter() *http.ServeMux {
 	m := http.NewServeMux()
 	m.HandleFunc("/weatherstation/updateweatherstation.php", app.stationUpdateHandler)
-	m.HandleFunc("/weather/update", app.weatherUpdateHandler)
-	m.HandleFunc("/weather", app.weatherHandler)
+
+	m.HandleFunc("/api/weather/update", app.weatherUpdateHandler)
+	m.HandleFunc("/api/weather", app.weatherHandler)
+
 	m.HandleFunc("/", app.defaultHandler)
 	return m
 }
